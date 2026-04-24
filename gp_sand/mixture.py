33ccd8857f76c8912d4c77f4b4563bfac43736e4
Clement jacquet <cjacquet@epri.com>
2026-04-24 12:49:40 +0200

    Add ModelList for mixtures GP

--- gp_sand/mixture.py
+++ gp_sand/mixture.py
@@ -10,7 +10,8 @@ from gpytorch.kernels import Kernel
 from gpytorch.likelihoods import _GaussianLikelihoodBase
 from gpytorch.means import Mean
 from gpytorch.mlls import MarginalLogLikelihood
-from gpytorch.models import ApproximateGP, GP
+from gpytorch.models import ApproximateGP, GP, IndependentModelList
+from gpytorch.utils.generic import length_safe_zip
 from gpytorch.variational import (
     CholeskyVariationalDistribution,
     VariationalStrategy
@@ -21,13 +22,16 @@ import numpy as np
 import pandas as pd
 
 import torch
-from torch import Tensor
+from torch import nn, Tensor
 from torch.distributions import Normal, MixtureSameFamily, Categorical
 from torch.optim import Adam
 
 
 # PACKAGE IMPORTS
-from gp_sand.metrics import compute_scores, display_scores
+from gp_sand.metrics import (
+    compute_scores,
+    # display_scores
+)
 from gp_sand.models import GPInterface, SCORES
 from gp_sand.utils import to_numpy, to_tensor
 
@@ -222,6 +226,73 @@ class MoEVariationalELBO(MarginalLogLikelihood):
         return elbo
 
 
+class SumMoEVariationalELBO(MarginalLogLikelihood):
+    """Sum of marginal MoE Variation ELBO, to be used with Multi-Output models.
+
+    Args:
+        likelihood: A MultiOutputLikelihood
+        model: A MultiOutputModel
+        mll_cls: The Marginal Log Likelihood class (default: ExactMarginalLogLikelihood)
+
+    In case the model outputs are independent, this provives the MLL of the multi-output model.
+
+    """
+
+    def __init__(
+        self,
+        likelihood: _GaussianLikelihoodBase,  # There for compatibility not used
+        model: 'MoESVGPList',
+        num_data: List[int],
+        beta: float | List[float] = 1.0,
+    ):
+        super().__init__(model.likelihood, model)
+        # NOTE: This hack is to by-pass the init of Super that does not allow
+        # to pass kwargs for the selected MLL model while MoEVariationalELBO
+        # requires the num_data parameters.
+        _verify_length(num_data, model.n_models)
+        beta = _make_list(beta, model.n_models)
+        _verify_length(beta, model.n_models)
+        self.mlls = nn.ModuleList([
+            MoEVariationalELBO(model.likelihood, model, num_data=nd, beta=b)
+            for model, nd, b in zip(model.models, num_data, beta)
+        ])
+
+    def forward(
+        self,
+        outputs: List[MultivariateNormal],
+        targets: List[Tensor],
+        params: List[Mapping[str, Any]] = []
+    ) -> Tensor:
+        """
+        Execute forward step.
+
+        Parameters
+        ----------
+        outputs: List[MultivariateNormal]
+            List of lists  of multivariate normals return by the model list.
+        targets: List[Tensor]
+            List of training targets.
+        params: List[Mapping[str, Any]]
+            List of optional parameters to pass to each ELBO MLL.
+
+        Returns
+        -------
+            Tensor
+        """
+        if len(params) == 0:
+            sum_mll = sum(
+                mll(*output, *target)
+                for mll, output, target in length_safe_zip(self.mlls, outputs, targets)
+            )
+        else:
+            sum_mll = sum(
+                mll(output, target, **iparams)
+                for mll, output, target, iparams in length_safe_zip(
+                    self.mlls, outputs, targets, params)
+            )
+        return sum_mll.div_(len(self.mlls))
+
+
 # MAIN CLASS
 class MixtureofExpertSVGP(GP, GPInterface):
     """Mixture of Expert combining prediction from individual SVGP \
@@ -260,15 +331,15 @@ class MixtureofExpertSVGP(GP, GPInterface):
         _verify_length(likelihoods, n_experts)
 
         # Set up the experts and likelihoods
-        self.experts: List[ExpertSVGP] = [
+        self.experts: nn.ModuleList = nn.ModuleList([
             ExpertSVGP(
                 ind_points[k],
                 means[k],
                 covars[k],
             )
             for k in range(n_experts)
-        ]
-        self.likelihoods = likelihoods
+        ])
+        self.likelihood = nn.ModuleList(likelihoods)
 
         # Gating
         sharpness_constraint = Positive()
@@ -386,12 +457,12 @@ class MixtureofExpertSVGP(GP, GPInterface):
 
         obs_means = torch.stack(
             [lik.marginal(lat).mean
-             for lik, lat in zip(self.likelihoods, latents)], dim=0
+             for lik, lat in zip(self.likelihood, latents)], dim=0
         )  # (K, N)
 
         obs_vars = torch.stack(
             [lik.marginal(lat).variance
-             for lik, lat in zip(self.likelihoods, latents)], dim=0
+             for lik, lat in zip(self.likelihood, latents)], dim=0
         )  # (K, N)
 
         mixture_mean = (weights * obs_means).sum(dim=0)
@@ -435,7 +506,7 @@ class MixtureofExpertSVGP(GP, GPInterface):
 
         Returns
         -------
-            BaseExactGP
+            MixtureofExpertSVGP
         """
         # Get defaults
         def _get_defaults() -> Mapping[str, Any]:
@@ -449,26 +520,32 @@ class MixtureofExpertSVGP(GP, GPInterface):
 
         # Set training mode
         self.train()
-        [model.train() for model in self.experts]
-        [likelihood.train() for likelihood in self.likelihoods]
+        self.likelihood.train()
+        # TODO: Remove the commented code
+        # [model.train() for model in self.experts]
+        # [likelihood.train() for likelihood in self.likelihood]
 
         # Setup optimizer
-        all_params = [
-            {'params': self.parameters()},
-        ]
-        for expert, llk in zip(self.experts, self.likelihoods):
-            all_params += [
-                {'params': expert.parameters()},
-                {'params': llk.parameters()}
-            ]
+        # all_params = [
+        #     {'params': self.parameters()},
+        # ]
+        # for expert, llk in zip(self.experts, self.likelihood):
+        #     all_params += [
+        #         {'params': expert.parameters()},
+        #         {'params': llk.parameters()}
+        #     ]
 
         optimizer = Adam(
-            all_params,
+            # all_params,
+            [
+                {'params': self.parameters()},
+                # {'params': self.likelihood.parameters()}
+            ],
             **(_get_defaults() | optim_kw)
         )
 
         # Set the objective function
-        mll = MoEVariationalELBO(self.likelihoods, self, num_data=train_y.size(0))
+        mll = MoEVariationalELBO(self.likelihood, self, num_data=train_y.size(0))
 
         # Start training loop
         for n in range(n_epochs):
@@ -501,7 +578,7 @@ class MixtureofExpertSVGP(GP, GPInterface):
                 logger.info(msg)
 
         # Display score on selected metrics
-        display_scores(self.score(train_x, train_y))
+        # display_scores(self.score(train_x, train_y))
 
         return self
 
@@ -534,8 +611,8 @@ class MixtureofExpertSVGP(GP, GPInterface):
 
         # Set to eval mode
         self.eval()
-        [model.eval() for model in self.experts]
-        [likelihood.eval() for likelihood in self.likelihoods]
+        # [model.eval() for model in self.experts]
+        # [likelihood.eval() for likelihood in self.likelihood]
 
         with torch.no_grad():
             mean, var, _ = self.mixture_distribution(test_x)
@@ -612,7 +689,7 @@ class IterativeTrimmingMofEtSVGP(MixtureofExpertSVGP):
         thresh_max: float | None = None,
         optim_kw: Mapping[str, Any] = {},
         verbose: bool = True,
-    ) -> 'MixtureofExpertSVGP':
+    ) -> 'IterativeTrimmingMofEtSVGP':
         """
         Given the traning data and fitting option, fit the model.
 
@@ -672,10 +749,313 @@ class IterativeTrimmingMofEtSVGP(MixtureofExpertSVGP):
                 ind = res <= upper
 
             logger.info(
-                f'{self.name} - Rejecting {-1 * (~ind.numpy().sum())} training samples '
+                f'{self.name} - Rejecting {len(ind) - ind.numpy().sum()} training samples '
                 f'({(100 * (1 - ind.numpy().mean())).round(2)}% of the data).'
             )
             train_x = train_x[ind, :]
             train_y = train_y[ind]
+
+        return self
+
+
+# MODEL LIST
+class MoESVGPList(IndependentModelList, GPInterface):
+    """
+    Independent list of Mixture of Expert (MoE) using Sparse Variational \
+        Gaussian Processes for convienent model fitting/predicting.
+
+    Notes
+    -----
+    This is a wrapper around GPyTorch `IndependentModelList` with a custom \
+        training function to handle the variational ELBO.
+
+    Attributes
+    ----------
+    models: List[MixtureofExpertSVGP]
+        The list of Mixture of Expert SVGPs.
+    """
+
+    def __init__(self, *models: List[MixtureofExpertSVGP]):
+        super().__init__(*models)
+
+    # Properties
+    @property
+    def name(self) -> str:
+        """Return the class name."""
+        return self.__class__.__name__
+
+    @property
+    def n_models(self) -> int:
+        """Return the number of models making the model list."""
+        return len(self.models)
+
+    # Utils
+    def validate_inputs(self, inputs: List[np.ndarray | Tensor]) -> None:
+        """
+        Given the list of inputs (feature or target), verify it matches with number \
+            of model and raise ValueError if otherwise.
+
+        Parameters
+        ----------
+        inputs: List[np.ndarray | Tensor]
+            Inputs to be tested.
+
+        Raises
+        ------
+            ValueError
+        """
+        len_inputs = len(inputs)
+        if len_inputs != self.n_models:
+            msg = f'Input length ({len_inputs}) does not match the number of models ' \
+                  f'in the list ({self.n_models}). Please check your inputs.'
+            logger.error(msg)
+            raise ValueError(msg)
+
+    # Fit/Predict
+    def fit(
+        self,
+        train_inputs: List[np.ndarray | Tensor],
+        train_targets: List[np.ndarray | Tensor],
+        n_epochs: int = 150,
+        optim_kw: Mapping[str, Any] = {},
+        verbose: bool = True,
+    ) -> 'MoESVGPList':
+        """
+        Given the traning data and fitting option, fit the model.
+
+        Parameters
+        ----------
+        train_x: List[np.ndarray | torch.Tensor]
+            List of tensors of training features.
+        train_y: List[np.ndarray | torch.Tensor]
+            List of tensors of training features.
+        n_epochs: int
+            Number of training epoch.
+        optim_kw: Mapping[str, Any]
+            A mapper of the form param_name -> param_value of optional \
+            settings for the optimizer.
+        verbose: bool
+            An option for whether to print taining status in logger.
+
+        Returns
+        -------
+            MoESVGPList
+        """
+        # Get defaults
+        def _get_defaults() -> Mapping[str, Any]:
+            """Get default settings"""
+            params = {'lr': .1}
+            return params
+
+        # Force input types
+        # TODO: Allow for mini-batching
+        train_inputs = [to_tensor(t) for t in train_inputs]
+        train_targets = [to_tensor(t) for t in train_targets]
+
+        # Set training mode
+        self.train()
+        self.likelihood.train()
+
+        optimizer = Adam(
+            [{'params': self.parameters()},],
+            **(_get_defaults() | optim_kw)
+        )
+
+        # Set the objective function
+        mll = SumMoEVariationalELBO(
+            self.likelihood,
+            self,
+            [train_y.size(0) for train_y in train_targets]
+        )
+
+        # Start training loop
+        for n in range(n_epochs):
+            # Zero grad
+            optimizer.zero_grad()
+
+            # Call
+            predictions = self(*train_inputs)
+            loss = -mll(
+                predictions,
+                train_targets,
+                params=[{'x': train_x} for train_x in train_inputs]
+            )
+
+            # Backward and propr
+            loss.backward()
+            optimizer.step()
+
+            if n == 0 or (n + 1) % 25 == 0 and verbose:
+                msg = (
+                    f'{self.name} - Iter {n + 1} of {n_epochs}: '
+                    f'Loss: {loss.item(): .3f}'
+                )
+                # weights = self.get_gates(train_x).mean(dim=1)
+                # msg += ' - '.join([
+                #     f' weight expert #{n:02d} = {round(weights[0].item(), 2)}'
+                #     for n in range(self.n_experts)
+                # ])
+                # msg += '\n'
+                # msg += ' - '.join([
+                #     f' noise expert #{n:02d} = {lkh.noise.item():.3e}'
+                #     for n, lkh in enumerate(self.likelihoods)
+                # ])
+                logger.info(msg)
+
+        # Display score on selected metrics
+        # display_scores(self.score(train_x, train_y))
+
+        return self
+
+    def predict(
+        self,
+        test_inputs: np.ndarray | Tensor | List[np.ndarray | Tensor],
+        return_ci: bool = True,
+        ci_width: float = 1.96
+    ) -> List[MultivariateNormal] | List[Tuple[MultivariateNormal, Tensor, Tensor]]:
+        """
+        Given the test features, make prediction and return the posterior \
+            distribution alongside with confidence interval.
+
+        Parameters
+        ----------
+        test_x: np.ndarray | Tensor | List[np.ndarray | Tensor]
+            Input features.
+        return_ci: bool
+            An option for whether to return the confidence interval.
+        ci_width: float
+            The width of the confidence interval in multiple of the standard \
+            deviation at each point. Default is 1.96 (90% confidence interval).
+
+        Returns
+        -------
+            MultivariateNormal | Tuple[MultivariateNormal, Tensor, Tensor]
+        """
+        # Force input types
+        _make_list(test_inputs, n_elems=self.n_models)
+        test_inputs = [to_tensor(test_x) for test_x in test_inputs]
+
+        # Set to eval mode
+        self.eval()
+        # [model.eval() for model in self.experts]
+        # [likelihood.eval() for likelihood in self.likelihood]
+
+        outputs = []
+        for model, test_x in length_safe_zip(self.models, test_inputs):
+            outputs.append(model.predict(test_x, return_ci, ci_width))
+
+        return outputs
+
+    def score(
+        self,
+        test_inputs: List[np.ndarray | Tensor],
+        test_targets: List[np.ndarray | Tensor],
+        methods: (
+            Callable[[np.ndarray], float]
+            | List[Callable[[np.ndarray], float]]
+        ) = SCORES
+    ) -> pd.DataFrame:
+        """Given the test features and target compute the corresponding \
+            prediction scores."""
+        scores = [
+            model.score(test_x, test_y)
+            for model, test_x, test_y in length_safe_zip(
+                self.models,
+                test_inputs,
+                test_targets
+            )
+        ]
+
+        return scores
+
+
+class ITMoESVGPList(MoESVGPList):
+    """
+    Independent list of Iterative Trimming Mixture of Expert (MoE) using Sparse \
+        Variational Gaussian Processes for convienent model fitting/predicting.
+
+    Notes
+    -----
+    This is a wrapper around GPyTorch `IndependentModelList` with a custom \
+        training function to handle the variational ELBO.
+
+    Attributes
+    ----------
+    models: List[MixtureofExpertSVGP]
+        The list of Mixture of Expert SVGPs.
+    """
+
+    # Fit/Predict
+    def fit(
+        self,
+        train_inputs: List[np.ndarray | Tensor],
+        train_targets: List[np.ndarray | Tensor],
+        n_iter: int = 5,
+        n_epochs: int = 100,
+        thresh_sd: float = 2.62,
+        thresh_max: float | None = None,
+        optim_kw: Mapping[str, Any] = {},
+        verbose: bool = True,
+    ) -> 'ITMoESVGPList':
+        """
+        Given the traning data and fitting option, fit the model.
+
+        Parameters
+        ----------
+        train_x: List[np.ndarray | torch.Tensor]
+            List of tensors of training features.
+        train_y: List[np.ndarray | torch.Tensor]
+            List of tensors of training features.
+        n_epochs: int
+            Number of training epoch.
+        optim_kw: Mapping[str, Any]
+            A mapper of the form param_name -> param_value of optional \
+            settings for the optimizer.
+        verbose: bool
+            An option for whether to print taining status in logger.
+
+        Returns
+        -------
+            MoESVGPList
+        """
+        for n in range(n_iter):
+            logger.info(f'{self.name} - Start training iteration {n + 1} (of {n_iter}).')
+
+            # Run training of the MoE
+            logger.info(f'{self.name} - MoE training step.')
+            super().fit(
+                train_inputs,
+                train_targets,
+                n_epochs=n_epochs,
+                optim_kw=optim_kw,
+                verbose=verbose,
+            )
+
+            # Run training of the MoE
+            logger.info(f'{self.name} - Outlier removal step.')
+            for m, model in enumerate(self.models):
+                train_x = train_inputs[m]
+                train_y = train_targets[m]
+                with torch.no_grad():
+                    mean, var, _ = model.mixture_distribution(train_x)
+                    upper = thresh_sd * var.sqrt()
+
+                    # Apply thresholding
+                    limit = [sys.maxsize, thresh_max][thresh_max is not None]
+                    upper = torch.where(
+                        upper <= limit,
+                        upper,
+                        limit
+                    )
+
+                    res = train_y - mean
+                    ind = res <= upper
+
+                logger.info(
+                    f'{self.name} - Rejecting {-1 * (~ind.numpy().sum())} training samples '
+                    f'({(100 * (1 - ind.numpy().mean())).round(2)}% of the data).'
+                )
+                train_inputs[m] = train_x[ind, :]
+                train_targets[m] = train_y[ind]
 
         return self
